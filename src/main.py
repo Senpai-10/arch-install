4f4d2e5117c24dd2828906f575470bf40c64c265
abdalrzag eisa <bmjfdrh@gmail.com>
2022-05-17 00:46:11 +0300

    main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print("arch installer3")
+from menu import Menu
+
+def main():
+    print("===================[ Archlinux Installer ]===================")
+    print("=    Author: senpai-10 <bmjfdrh@gmail.com>            =")
+    print("=    GitHub profile: https://github.com/senpai-10     =")
+    print("============================================================\n")
+    
+    main_menu = Menu(["choice 1", "choice 2", "choice 3"]).choice
+    print(f"choice: {main_menu}")
+    
+    if main_menu == "choice 1":
+        print("hi")
+
+if __name__ == '__main__':
+    main()
