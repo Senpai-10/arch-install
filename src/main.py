0a07c7b8e5495d47f828e318ed5b06fab6685ff5
abdalrzag eisa <bmjfdrh@gmail.com>
2022-05-17 00:49:25 +0300

    main

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@ from menu import Menu
 
 def main():
     print("===================[ Archlinux Installer ]===================")
-    print("=    Author: senpai-10 <bmjfdrh@gmail.com>            =")
-    print("=    GitHub profile: https://github.com/senpai-10     =")
-    print("============================================================\n")
+    print("=                                                           =")
+    print("=        Author: senpai-10 <bmjfdrh@gmail.com>              =")
+    print("=        GitHub profile: https://github.com/senpai-10       =")
+    print("=                                                           =")
+    print("=============================================================\n")
     
     main_menu = Menu(["choice 1", "choice 2", "choice 3"]).choice
     print(f"choice: {main_menu}")
