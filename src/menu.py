fb747f6331df0b38b637137243dab2928842f140
abdalrzag eisa <bmjfdrh@gmail.com>
2022-05-18 01:14:46 +0300

    fix invaild input

--- src/menu.py
+++ src/menu.py
@@ -1,3 +1,4 @@
+from ctypes import cast
 from colors import bright_green, bright_yellow
 
 class Menu:
@@ -20,14 +21,23 @@ class Menu:
         
         for i in range(len(self.__menu)):
             print(f"[{bright_green(i+1)}] {self.__menu[i]}")
+            
+        print()
         
-        choice = int(input("\nchoice: "))
+        while True:
+            choice_input = input("choice: ")
+            
+            if (not choice_input.isnumeric() or 
+                int(choice_input) > len(self.__menu) or 
+                int(choice_input) < 0):
+                print("Index is out of bounds!")
+                continue
+            
+            break
+            
+        choice_index = int(choice_input)
         
-        if choice > len(self.__menu) or choice < 0:
-            print("Index is out of bounds!")
-            exit(1)
-        
-        if choice < 1:
-            self.choice = self.__menu[choice]
-        else: self.choice = self.__menu[choice-1]
+        if choice_index < 1:
+            self.choice = self.__menu[choice_index]
+        else: self.choice = self.__menu[choice_index-1]
         
