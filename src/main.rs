2ffdd63b7293d8121f4e6b69c1ec809b6b01b1fe
abdalrzag eisa <bmjfdrh@gmail.com>
2022-09-18 21:06:23 +0300

    main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-mod config;
-mod enums;
+mod settings;
+mod setup;
+mod constants;
 
-use config::Config;
+use setup::get_settings;
 
 fn main() {
-    println!("config: {}", Config::get().partitioning.as_str());
+    let settings: settings::Settings = get_settings();
+
+    println!("hostname '{}'", settings.hostname);
+    println!("username '{}'", settings.username);
+    println!("timezone '{}'", settings.timezone);
 }
