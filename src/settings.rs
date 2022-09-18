1d1b2206310b46fa3f7be7cbfd6706c938f9419d
abdalrzag eisa <bmjfdrh@gmail.com>
2022-09-18 20:52:39 +0300

    settings

---
+++ src/settings.rs
@@ -0,0 +1,40 @@
+pub struct Settings {
+    pub hostname: String,
+    pub root_password: String,
+    pub username:  String,
+    pub user_password:  String,
+    pub timezone: String,
+    pub keymap: String,
+
+    pub partition_table_format: String,
+    pub drive: String,
+    
+    // user apps
+    pub files_manager: String,
+}
+
+impl Settings {
+    pub fn new() -> Self {
+        Settings {
+            hostname: String::from(""),
+            root_password: String::from(""),
+            username: String::from(""),
+            user_password: String::from(""),
+            timezone: String::from("Asia/Riyadh"),
+            keymap: String::from("us"),
+
+            /*
+                options:
+                    1. mbr
+                    2. gpt
+            */
+            partition_table_format: String::from("mbr"),
+
+            // Main installation drive name
+            // Example: sda without '/dev/'
+            drive: String::from(""),
+
+            files_manager: String::from("nemo"),
+        }
+    }
+}
