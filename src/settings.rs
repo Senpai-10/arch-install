cfe94d933ae007472cdec6c10da417bb692f7528
abdalrzag eisa <bmjfdrh@gmail.com>
2022-09-20 13:10:03 +0300

    remove impl

--- src/settings.rs
+++ src/settings.rs
@@ -12,29 +12,3 @@ pub struct Settings {
     // user apps
     pub files_manager: String,
 }
-
-impl Settings {
-    pub fn new() -> Self {
-        Settings {
-            hostname: String::from(""),
-            root_password: String::from(""),
-            username: String::from(""),
-            user_password: String::from(""),
-            timezone: String::from("Asia/Riyadh"),
-            keymap: String::from("us"),
-
-            /*
-                options:
-                    1. mbr
-                    2. gpt
-            */
-            partitioning_scheme: String::from("mbr"),
-
-            // Main installation drive name
-            // Example: sda without '/dev/'
-            drive: String::from(""),
-
-            files_manager: String::from("nemo"),
-        }
-    }
-}
