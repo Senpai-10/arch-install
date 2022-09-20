efb578054314aba9dcd91ca98c639cc39a1bfac1
abdalrzag eisa <bmjfdrh@gmail.com>
2022-09-20 18:54:45 +0300

    wrap get_settings and printall and confirm

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
+#[macro_use] extern crate prettytable;
+
 mod settings;
 mod setup;
 mod constants;
+mod censor_password;
 
 use setup::get_settings;
 
-fn main() {
-    let settings: settings::Settings = get_settings();
+use dialoguer::Confirm;
+use dialoguer::theme::ColorfulTheme;
 
-    println!("hostname '{}'", settings.hostname);
-    println!("username '{}'", settings.username);
-    println!("timezone '{}'", settings.timezone);
+fn main() {
+    let theme = ColorfulTheme::default();
+    let mut settings: settings::Settings;
+
+    loop {
+        settings = get_settings(&theme);
+
+        settings.print_all();
+
+        let continue_script = Confirm::with_theme(&theme)
+            .with_prompt("Do you want to continue")
+            .default(true)
+            .show_default(true)
+            .interact().unwrap();
+
+        if continue_script {
+            break;
+        }
+    }
+
+    println!("after: gs{}", settings.hostname);
 }
