9ec1685d7565fc1c04c6c33840dfa0c0f16da906
abdalrzag eisa <bmjfdrh@gmail.com>
2022-09-23 00:06:57 +0300

    mod pacman

--- src/helpers.rs
+++ src/helpers.rs
@@ -28,6 +28,41 @@ pub mod pacman {
     
         status
     }
+
+    /// pacman config
+    /// 
+    /// Uncomment `ParallelDownloads`, and change number 
+    /// 
+    /// file: `/etc/pacman.conf`
+    pub fn set_parallel_downloads(n: usize) {
+        // put the number in the regex
+        let sed_regex = format!("s/^#ParallelDownloads = 5$/ParallelDownloads = {}/", n);
+
+        Command::new("sed")
+        .args([
+            "-i", 
+            &sed_regex,
+            "/etc/pacman.conf"])
+        .status().unwrap();
+
+        info!("pacman.conf: ParallelDownloads is set to '{}'", n);
+    }
+
+    /// pacman config
+    /// 
+    /// Uncomment [multilib], and 'Include' line
+    /// 
+    /// file: `/etc/pacman.conf`
+    pub fn enable_multilib() {
+        Command::new("sed")
+        .args([
+            "-i", 
+            "\"/\\[multilib\\]/,/Include/\"'s/^#//'",
+            "/etc/pacman.conf"])
+        .status().unwrap();
+
+        info!("pacman.conf: multilib enabled");
+    }
 }
 
 fn convert_vector_of_bytes_to_string(bytes: Vec<u8>) -> String {
