9bb120db2e2af06f3df52a9ccad53f94d1dddf64
abdalrzag eisa <bmjfdrh@gmail.com>
2022-09-30 15:33:40 +0300

    genfstab

--- src/installation_parts/configure_the_system.rs
+++ src/installation_parts/configure_the_system.rs
@@ -1,13 +1,18 @@
 use crate::settings::Settings;
-use std::process::{Command};
+use std::process::{Command, Stdio};
+use execute::{Execute, shell};
+use std::fs::File;
 
 pub fn configure_the_system(settings: &Settings) {
     // genfstab -U /mnt >> /mnt/etc/fstab
+
+    let file = File::create("/mnt/etc/fstab").unwrap();
+    let stdio = Stdio::from(file);
+
     Command::new("genfstab")
         .arg("-U")
         .arg("/mnt")
-        .arg(">>")
-        .arg("/mnt/etc/fstab")
+        .stdout(stdio)
         .status()
         .unwrap();
 }
