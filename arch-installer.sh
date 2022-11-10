78b108f5bf53d484b729e278009f16338507a7cb
abdalrzag eisa <bmjfdrh@gmail.com>
2022-11-10 15:24:22 +0300

    cd .dotfiles

--- arch-installer.sh
+++ arch-installer.sh
@@ -396,7 +396,7 @@ function fn_post_installation {
 
     git clone https://github.com/Senpai-10/dotfiles.git .dotfiles
 
-    cd .dotfiles/management-scripts/
+    cd .dotfiles/ || :
 
     ./bootstrap.sh
 
